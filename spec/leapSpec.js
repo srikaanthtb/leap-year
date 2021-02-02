2607c81efbbef7f2aae37aed3d7ced757f783ae6
srikaanthtb <srikaanth.t.b@gmail.com>
2021-02-02 18:22:51 +0000

    added 2 more tests

--- spec/leapSpec.js
+++ spec/leapSpec.js
@@ -2,5 +2,11 @@ describe('leapyear', function(){
 
     it('should say 2000 is a leap year',function(){
         expect(leap(2000)).toEqual('leap year');
-    })
+    });
+    it('should say 1900 is not a leap year',function(){
+        expect(leap(1900)).toEqual('not a leap year');
+    });
+    it('should say 2004 is a leap year',function(){
+        expect(leap(2004)).toEqual('leap year');
+    });
 })
--- src/leap.js
+++ src/leap.js
@@ -1,7 +1,9 @@
 function leap(year) {
     if (year % 400 == 0) {
         return 'leap year'
-    } else if {
-        
+    } else if (year % 100 == 0 && year % 400 != 0) {
+        return 'not a leap year'
+    } else if (year % 4 == 0 && year % 100 != 0){
+        return 'leap year'
     }
 }
