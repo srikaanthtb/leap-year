7aecebf532405acb2d723c4bd1b6694510074f64
srikaanthtb <srikaanth.t.b@gmail.com>
2021-02-02 18:12:18 +0000

    initial commit

---
+++ spec/leapSpec.js
@@ -0,0 +1,6 @@
+describe('leapyear', function(){
+
+    it('should say 2000 is a leap year',function(){
+        expect(leap(2000)).toEqual('leap year');
+    })
+})
---
+++ src/leap.js
@@ -0,0 +1,7 @@
+function leap(year) {
+    if (year % 400 == 0) {
+        return 'leap year'
+    } else if {
+        
+    }
+}
